9fab3d03e01da48871eac0b36276741b18f4c421
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-09 12:05:14 -0300

    Recebendo propriedades e adicionando Hooks

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,76 @@
-import React from 'react';
-import {TextField, Button, Switch, FormControlLabel} from '@material-ui/core';
+import React, {useState} from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function FormularioCadastro(){
+function FormularioCadastro({aoEnviar}) {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promocoes, setPromocoes] = useState(true);
+    const [novidades, setNovidades] = useState(true);
 
-    return(
-    <form>
-         <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
-         <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
-         <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
-
-        <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary" />} />
-        <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked color="primary" />} />
+  return (
+    <form onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({nome, sobrenome, cpf, promocoes, novidades})
         
-        <Button type="submit" variant="contained" color="primary" >Cadastrar</Button>
-    </form>);
+    }}>
+      <TextField
+      value={nome}
+      onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+      value={sobrenome}
+      onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={cpf}
+        onChange={(event) => {
+            setCpf(event.target.value);
+        }}
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch 
+            checked={promocoes}
+            onChange={(event)=>{
+            setPromocoes(event.target.checked)
+        }} name="promocoes"  color="primary" />}
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch  
+            checked={novidades}
+            onChange={(event)=>{
+            setNovidades(event.target.checked)
+        }}  
+            name="novidades"  color="primary" />}
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
-export default FormularioCadastro; 
+export default FormularioCadastro;
