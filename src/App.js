10ff58ddaeeaed029251584013ad99405af1dfd5
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-09 10:08:21 -0300

    Criando e Estilizando o formulário

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import 'fontsource-roboto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Container, Typography} from "@material-ui/core";
+
+class App extends Component {
+  render(){
+
+    return (
+      <Container component="article" maxWidth="sm" >
+        <Typography variant="h3" component="h1" align="center">Formulário de Cadastro </Typography>
+        <FormularioCadastro />
+      </Container >
+    );
+  }
 }
 
 export default App;
